f132eddfb2e6c62baad7761e15212e5f87cefa83
Marcus Simonsen <marcus@itis.dk>
2026-09-22 15:20:06 +0200

    add rewrite, judgments, multi-step-rewrite, and example. Also remove values

--- main.v
+++ main.v
@@ -29,7 +29,72 @@ Inductive Predicate : Type :=
   | Not (p : Predicate)
 .
 
-Inductive Value : Expression -> Type :=
-  | VNat (n : nat) :
-      Value (Nat n)
+Definition Destination := nat.
+
+Inductive Frame : Type :=
+  | Fe (d : Destination) (e : Expression)
+  | Fv (n : nat) (d : Destination)
 .
+
+Inductive Judgment : Type :=
+  | ev (e : Expression) (d : Destination)
+  | ret (n : nat) (d : Destination)
+  | frame (f : Frame) (d : Destination)
+.
+
+Notation "'[' x ']'" := (x :: nil).
+
+Inductive Rewrite : list Judgment -> list Judgment -> Prop :=
+  | evnat (v : nat) (d : Destination) :
+    Rewrite [ev (Nat v) d] [ret v d]
+  | evadd1 (e1 e2 : Expression) (d d1 : Destination) :
+    Rewrite [ev (Plus e1 e2) d] ((ev e1 d1) :: [frame (Fe d1 e2) d])
+  | evadd2 (n : nat) (e2 : Expression) (d d1 d2 : Destination) :
+      Rewrite ((ret n d1) :: [frame (Fe d1 e2) d]) (ev e2 d2 :: [frame (Fv n d2) d])
+  | evadd3 (n1 n2 : nat) (d d2 : Destination) :
+      Rewrite (ret n2 d2 :: [frame (Fv n1 d2) d]) [ret (n1 + n2) d]
+.
+
+Inductive MultiStepRewrite : list Judgment -> list Judgment -> Prop :=
+  | MSRefl (j : list Judgment) :
+      MultiStepRewrite j j
+  | MSStep (j1 j2 : list Judgment) :
+      Rewrite j1 j2 -> MultiStepRewrite j1 j2
+  | MSTrans (j1 j2 j3 : list Judgment) :
+      MultiStepRewrite j1 j2 ->
+      MultiStepRewrite j2 j3 ->
+      MultiStepRewrite j1 j3
+  | MSHead (j1 j1' : Judgment) (js : list Judgment) :
+      MultiStepRewrite [j1] [j1'] ->
+      MultiStepRewrite (j1 :: js) (j1' :: js)
+.
+
+Example n : nat := 3.
+
+Example e : Expression := Nat n.
+
+Example d : Destination := 0.
+Example d1 : Destination := 1.
+Example d2 : Destination := 2.
+
+Example ex1 : Judgment := ev e d.
+
+Example ret1 : Judgment := ret n d.
+
+Example rewrite1 : Rewrite [ex1] [ret1].
+Proof.
+  apply evnat.
+Qed.
+
+Example rewrite2 : MultiStepRewrite [ev (Plus (Nat 3) (Nat 4)) d] [ret 7 d].
+Proof.
+  apply MSTrans with (j2 := ev (Nat 3) d1 :: frame (Fe d1 (Nat 4)) d :: nil).
+  - apply MSStep. apply evadd1.
+  - apply MSTrans with (j2 := ret n d1 :: frame (Fe d1 (Nat 4)) d :: nil).
+    + apply MSHead. apply MSStep. apply evnat.
+    + apply MSTrans with (j2 := ev (Nat 4) d2 :: [frame (Fv 3 d2) d]).
+      * apply MSStep. apply evadd2.
+      * apply MSTrans with (j2 := ret 4 d2 :: [frame (Fv 3 d2) d]).
+        -- apply MSHead. apply MSStep. apply evnat.
+        -- apply MSStep. apply evadd3.
+Qed.
